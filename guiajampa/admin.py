a82f95c10f20ef848779fd72da5e7effd3a1130b
caio <caiolsilva10@gmail.com>
2026-05-19 21:49:00 -0300

    Adicionado modelo PontoTuristico, registro no admin, atualização do requirements e migração inicial

--- guiajampa/admin.py
+++ guiajampa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PontoTuristico
 
-# Register your models here.
+
+@admin.register(PontoTuristico)
+class PontoTuristicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'categoria', 'latitude', 'longitude')
+    search_fields = ('nome', 'categoria')
--- guiajampa/models.py
+++ guiajampa/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PontoTuristico(models.Model):
+    nome = models.CharField('Nome', max_length=200)
+    descricao = models.TextField('Descrição', blank=True)
+    latitude = models.DecimalField('Latitude', max_digits=9, decimal_places=6)
+    longitude = models.DecimalField('Longitude', max_digits=9, decimal_places=6)
+    foto = models.ImageField('Foto', upload_to='pontos/', blank=True, null=True)
+    categoria = models.CharField('Categoria', max_length=100)
+
+    def __str__(self):
+        return self.nome
